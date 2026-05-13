52c16e004829f2e7f3556cfe9683794e10782d87
Cuchin2 <luis.lucho3210@gmail.com>
2026-05-13 12:44:28 -0500

    Marca slug

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -10,6 +10,7 @@ class Brand extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'code',
         'image',
         'description',
@@ -19,7 +20,23 @@ class Brand extends Model
     protected $casts = [
         'order' => 'integer',
     ];
+    // Auto-generate slug from name when creating
+    protected static function boot()
+    {
+        parent::boot();
 
+        static::creating(function ($brand) {
+            if (empty($brand->slug)) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        });
+
+        static::updating(function ($brand) {
+            if ($brand->isDirty('name') && !$brand->isDirty('slug')) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        });
+    }
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
