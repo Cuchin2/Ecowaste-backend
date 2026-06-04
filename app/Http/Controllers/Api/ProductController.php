bf0c79af63e6723b21bd80e573a47c8480df3da7
Cuchin2 <luis.lucho3210@gmail.com>
2026-06-04 17:41:38 -0500

    show Tags

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -66,7 +66,7 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-        $product->load(['category', 'brand']);
+        $product->load(['category', 'brand', 'tags']);
         return response()->json($this->format($product));
     }
 
