1cb432f773a5170822ab9016843a7c67b1ddd358
Cuchin2 <luis.lucho3210@gmail.com>
2026-07-07 15:46:49 -0500

    v3

--- app/Http/Controllers/Api/WishlistController.php
+++ app/Http/Controllers/Api/WishlistController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\WishlistItem;
-use App\Models\Cartitem;
+use App\Models\CartItem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str; // 👈 importar Str
