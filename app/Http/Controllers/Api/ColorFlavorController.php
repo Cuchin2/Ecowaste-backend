d77eea265efb42e15f4a7c87abc7e0ba32aa4b38
Cuchin2 <luis.lucho3210@gmail.com>
2026-07-15 18:58:26 -0500

    cf

--- app/Http/Controllers/Api/ColorFlavorController.php
+++ app/Http/Controllers/Api/ColorFlavorController.php
@@ -12,7 +12,7 @@ class ColorFlavorController extends Controller
 {
     public function index()
     {
-        $items = ColorFlavor::orderBy('type')->ordered()->get();
+        $items = ColorFlavor::ordered()->get();
         return response()->json($items);
     }
 
