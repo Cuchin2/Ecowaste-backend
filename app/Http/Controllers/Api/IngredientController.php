15baea5c19a2f8b9cd10486538a1357453ff3222
Cuchin2 <luis.lucho3210@gmail.com>
2026-07-16 13:37:01 -0500

    ingredientes v4

--- app/Http/Controllers/Api/IngredientController.php
+++ app/Http/Controllers/Api/IngredientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Ingredient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IngredientController extends Controller
 {
